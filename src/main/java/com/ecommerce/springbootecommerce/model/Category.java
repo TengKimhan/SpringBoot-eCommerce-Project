bec0e26ffaeb16a388e9c3aa5b9acc29fcf6b565
TengKimhan <tengkimhan@gmail.com>
2026-07-24 14:59:15 +0930

    Managing category id

--- src/main/java/com/ecommerce/springbootecommerce/model/Category.java
+++ src/main/java/com/ecommerce/springbootecommerce/model/Category.java
@@ -1,15 +1,15 @@
 package com.ecommerce.springbootecommerce.model;
 
 public class Category {
-    private long categoryId;
+    private Long categoryId;
     private String categoryName;
 
-    public Category(long categoryId, String categoryName) {
+    public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
     }
 
-    public long getCategoryId() {
+    public Long getCategoryId() {
         return categoryId;
     }
 
