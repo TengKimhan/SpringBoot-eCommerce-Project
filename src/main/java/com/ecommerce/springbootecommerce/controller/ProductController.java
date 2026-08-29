94d0aca97fce6c1d3d88dea4d2a4a45a8bd26c75
TengKimhan <tengkimhan@gmail.com>
2026-08-29 15:34:10 +0930

    Setting up Product module

---
+++ src/main/java/com/ecommerce/springbootecommerce/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.ecommerce.springbootecommerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+}
